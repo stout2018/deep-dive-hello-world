965f78a91d31ebae7429d5e71e0b3e6f64db1a5f
ampki_000 <ampki_000@10.0.0.65>
2017-12-26 18:08:50 -0700

    Rewritten to match exactly

--- src/edu/cnm/deepdive/helloworld/HelloWorld.java
+++ src/edu/cnm/deepdive/helloworld/HelloWorld.java
@@ -4,13 +4,13 @@
 package edu.cnm.deepdive.helloworld;
 
 /**
- *Simple class that displays the "Hello World!" message.
+ * Simple class that displays the "Hello World!" message.
  */
 public class HelloWorld {
 
   /**
-   *  Prints "Hello World!" to standard output.
-   *  
+   * Prints "Hello World!" to standard output.
+   * 
    * @param args    Command-line parameters (ignored).
    */
   public static void main(String[] args) {
