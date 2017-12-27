d0a0c1d61d6ea93ded4aacfdaaff08b597a0127c
ampki_000 <ampki_000@10.0.0.65>
2017-12-26 17:29:46 -0700

    Implemented program to read "Hello World!" message

---
+++ src/edu/cnm/deepdive/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.helloworld;
+
+/**
+ * @author ampki_000
+ *Class for displaying "Hello World!" message.
+ */
+public class HelloWorld {
+
+  /**
+   *  Prints "Hello World!": to standard output.
+   * @param args    Command-line parameters (ignored)
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
+}
