af63a128317d461ac22ce911678a7a9cdcca9a6b
ampki_000 <ampki_000@10.0.0.65>
2017-12-26 17:43:06 -0700

    Edited comments line

--- src/edu/cnm/deepdive/helloworld/HelloWorld.java
+++ src/edu/cnm/deepdive/helloworld/HelloWorld.java
@@ -4,12 +4,13 @@
 package edu.cnm.deepdive.helloworld;
 
 /**
- *Class for displaying "Hello World!" message.
+ *Simple class that displays the "Hello World!" message.
  */
 public class HelloWorld {
 
   /**
    *  Prints "Hello World!" to standard output.
+   *  
    * @param args    Command-line parameters (ignored).
    */
   public static void main(String[] args) {
