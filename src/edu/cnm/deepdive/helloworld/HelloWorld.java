515e69469214c5ea8a8efcc7f9d7170e997969ce
stout2018 <donnystout47@gmail.com>
2018-01-22 21:39:38 -0700

    Changed the method and removed comments.

--- src/edu/cnm/deepdive/helloworld/HelloWorld.java
+++ src/edu/cnm/deepdive/helloworld/HelloWorld.java
@@ -3,18 +3,13 @@
  */
 package edu.cnm.deepdive.helloworld;
 
-/**
- * Simple class that displays the "Hello World!" message.
- */
 public class HelloWorld {
 
-  /**
-   * Prints "Hello World!" to standard output.
-   * 
-   * @param args    Command-line parameters (ignored).
-   */
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    emitMessage("Hello World!");
   }
 
+  private static void emitMessage(String message) {
+    System.out.println(message);
+  }
 }
